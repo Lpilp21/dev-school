fc4c0312e734d0c40d7ff33938519a462f3da985
levani <lpilp21@freeuni.edu.ge>
2023-04-22 15:48:02 +0400

    mortgage calc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MortgageCalc mg = new MortgageCalc();
+        System.out.println(mg.calculateMortgage());
     }
 }
